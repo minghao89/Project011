ee49c30180c1328ce3af7ea72cb7d043101557a9
minghao89 <877712787@qq.com>
2020-12-30 13:22:51 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,14 @@
+package com.atguigu.bean;
+
+/**
+ * @author Minghao
+ * @date 2020.12.29 - 20:49
+ */
+public class Customer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+
+    }
+
+}
